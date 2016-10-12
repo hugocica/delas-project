826ad020e37e18d3c0ce53c327b04ed642f668de
Hugo Cicarelli <hcicarelli@gmail.com>
2016-10-11 22:51:54 -0300

    Instagram API working

--- wp-content/themes/delas-theme/functions.php
+++ wp-content/themes/delas-theme/functions.php
@@ -23,27 +23,31 @@ add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 // registra o menu primário
 register_nav_menu( 'primary-menu', 'Primary Menu' );
 
-function get_insta_posts( $user_id ) {
-    // $insta_token = '808c80f73b5b474486b039875a6f2ec2';
-	// // $insta_user = '1173194129'; //bealpriscila
-	// // $insta_user = '31520384'; //hugo_cica
-	// $insta_url = 'https://api.instagram.com/v1/users/self/media/recent?access_token='. $insta_token;
-    // // $insta_url = 'https://api.instagram.com/v1/users/<user-id>/media/recent/';
-	//
-	// $process = curl_init();
-    // curl_setopt($process, CURLOPT_URL, $insta_url);
-    // curl_setopt($process, CURLOPT_SSL_VERIFYPEER, 0);
-    // curl_setopt($process, CURLOPT_HEADER, 1);
-    // curl_setopt($process, CURLOPT_TIMEOUT, 30);
-    // curl_setopt($process, CURLOPT_POST, 1);
-    // curl_setopt($process, CURLOPT_CUSTOMREQUEST, "GET");
-    // curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
-    // $return = curl_exec($process);
-    // curl_close($process);
-    //
-    // echo '<pre>';
-    // var_dump($return);
-    // echo '</pre>';
+function get_insta_posts() {
+    $client_id = "b371224661e14e0b8e80c53e0ad19525";
+    $token = "31520384.b371224.9d8ee2fd2cfa4b62a5a054729dd0084f";
+
+    // $insta_user = "1173194129"; //bealpriscila
+    // $insta_user = "31520384"; //hugo_cica
+
+    $url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=" . $token;
+    // $url = 'https://api.instagram.com/v1/users/1173194129/media/recent/';
+
+    $process = curl_init();
+    curl_setopt($process, CURLOPT_URL, $url);
+    curl_setopt($process, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($process, CURLOPT_HEADER, 0);
+    curl_setopt($process, CURLOPT_TIMEOUT, 30);
+    curl_setopt($process, CURLOPT_POST, 1);
+    curl_setopt($process, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
+    $return = curl_exec($process);
+    $error = curl_error($process);
+    curl_close($process);
+    
+    $response = json_decode($return);
+    
+    return $response;
 }
 
 include_once get_template_directory().'/metaboxes/setup.php';
--- wp-content/themes/delas-theme/page-templates/template-episodios.php
+++ wp-content/themes/delas-theme/page-templates/template-episodios.php
@@ -9,31 +9,23 @@
  ?>
 <?php echo get_header(); ?>
 <?php
-    // $episodios = get_insta_posts( '1173194129' );
+    $episodios = get_insta_posts();
 ?>
 
 <div class="container">
     <?php
-    $insta_token = '808c80f73b5b474486b039875a6f2ec2';
-    // $insta_user = '1173194129'; //bealpriscila
-    // $insta_user = '31520384'; //hugo_cica
-    $insta_url = 'https://api.instagram.com/v1/users/self/media/recent?access_token='. $insta_token;
-    // $insta_url = 'https://api.instagram.com/v1/users/<user-id>/media/recent/';
+        echo '<div class="episodes-container col-md-12">';
+        foreach ( $episodios->data as $data ) {
+            ?>
+                <div class="episodes-item col-md-4">
+                    <img width="100%" src="<?php echo $data->images->standard_resolution->url; ?>">
+                </div>
+            <?php
+            //var_dump($data); // {'attribution', 'tags', 'type', 'location', 'comments:[count]', 'filter', 'created_time', 'link', 'likes:[count]', 'images:[low resolution, thumbnail, standart_resolution:[url, width, height]]'}
+        }
+        echo '</div>';
 
-    $process = curl_init();
-    curl_setopt($process, CURLOPT_URL, $insta_url);
-    curl_setopt($process, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($process, CURLOPT_HEADER, 1);
-    curl_setopt($process, CURLOPT_TIMEOUT, 30);
-    curl_setopt($process, CURLOPT_POST, 1);
-    curl_setopt($process, CURLOPT_CUSTOMREQUEST, "GET");
-    curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
-    $return = curl_exec($process);
-    curl_close($process);
-
-    echo '<pre>';
-    var_dump($return);
-    echo '</pre>';
+        // $episodios->data
     ?>
 </div>
 
