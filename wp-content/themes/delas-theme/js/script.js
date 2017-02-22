8ffcc6b57704abc67f9492d7bde7824d2164afa1
Hugo Cicarelli <hcicarelli@gmail.com>
2017-02-22 03:12:50 -0300

    Menu Mobile

--- wp-content/themes/delas-theme/js/script.js
+++ wp-content/themes/delas-theme/js/script.js
@@ -1,8 +1,10 @@
 jQuery(document).ready(function($) {
 	$('button.navbar-toggle').bind('click', function(event) {
 		if ( $('.main-navigation-bar').hasClass('opened') ) {
+			$(this).removeClass('opened');
 			$('.main-navigation-bar').removeClass('opened');
 		} else {
+			$(this).addClass('opened');
 			$('.main-navigation-bar').addClass('opened');
 		}
 	});
